3984bfecf58f94a9742145248f7fa436c73e0383
Pimug <pimug77254.7@gmail.com>
2025-09-30 23:00:58 +0700

    first commit

---
+++ .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/pimug/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/pimug/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
